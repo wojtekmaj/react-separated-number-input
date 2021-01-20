15d7407fd5642cbccd77eadefc52811a36ea061c
Wojciech Maj <kontakt@wojtekmaj.pl>
2021-01-20 20:49:13 +0100

    Rewrite test suite to React Hooks

--- test/Test.jsx
+++ test/Test.jsx
@@ -1,71 +1,65 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import SeparatedNumberInput from 'react-separated-number-input/src/entry';
 
 import './Test.less';
 
 const initialValue = '12345678900';
 
-export default class Test extends Component {
-  state = {
-    value: initialValue,
+export default function Test() {
+  const [value, setValue] = useState(initialValue);
+
+  function onChange(event) {
+    setValue(event.target.value);
   }
 
-  onChange = (event) => {
-    this.setState({ value: event.target.value });
-  }
-
-  render() {
-    const { value } = this.state;
-
-    return (
-      <div className="Test">
-        <header>
-          <h1>
-            react-separated-number-input test page
-          </h1>
-        </header>
-        <div className="Test__container">
-          <aside className="Test__container__options" />
-          <main className="Test__container__content">
-            <form
-              onSubmit={(event) => {
-                event.preventDefault();
-                /* eslint-disable no-console */
-                console.error('SeparatedNumberInput triggered submitting the form.');
-                console.log(event);
-                /* eslint-enable no-console */
-              }}
+  return (
+    <div className="Test">
+      <header>
+        <h1>
+          react-separated-number-input test page
+        </h1>
+      </header>
+      <div className="Test__container">
+        <aside className="Test__container__options" />
+        <main className="Test__container__content">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              /* eslint-disable no-console */
+              console.error('SeparatedNumberInput triggered submitting the form.');
+              console.log(event);
+              /* eslint-enable no-console */
+            }}
+          >
+            <p>Controlled:</p>
+            <SeparatedNumberInput
+              groupLengths={[2, 3, 3, 3]}
+              onChange={onChange}
+              value={value}
+            />
+            <p>Uncontrolled:</p>
+            <SeparatedNumberInput
+              defaultValue={initialValue}
+              groupLengths={[2, 3, 3, 3]}
+              onChange={onChange}
+            />
+            <p>With pattern:</p>
+            <SeparatedNumberInput
+              defaultValue={initialValue}
+              groupLengths={[2, 3, 3, 3]}
+              pattern="^[0-9]{2} [0-9]{3} [0-9]{3} [0-9]{3}$"
+            />
+            <br />
+            <br />
+            <button
+              id="submit"
+              type="submit"
             >
-              <p>Controlled:</p>
-              <SeparatedNumberInput
-                groupLengths={[2, 3, 3, 3]}
-                onChange={this.onChange}
-                value={value}
-              />
-              <p>Uncontrolled:</p>
-              <SeparatedNumberInput
-                defaultValue={initialValue}
-                groupLengths={[2, 3, 3, 3]}
-                onChange={this.onChange}
-              />
-              <p>With pattern:</p>
-              <SeparatedNumberInput
-                defaultValue={initialValue}
-                groupLengths={[2, 3, 3, 3]}
-                pattern="^[0-9]{2} [0-9]{3} [0-9]{3} [0-9]{3}$"
-              />
-              <br />
-              <br />
-              <button
-                id="submit"
-                type="submit"
-              >
-                Submit
-              </button>
-            </form>
-          </main>
-        </div>
+              Submit
+            </button>
+          </form>
+        </main>
       </div>
-    );
-  }
+    </div>
+  );
 }
